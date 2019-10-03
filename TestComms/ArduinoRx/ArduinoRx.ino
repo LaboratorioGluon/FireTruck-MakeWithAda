3b42c374704fe1f2c2590fc6df218bef3d4af207
Angel <laboratoriogluon@gmail.com>
2019-10-03 08:32:41 +0200

    Examples for the SPI - NRF24 transmision

---
+++ TestComms/ArduinoRx/ArduinoRx.ino
@@ -0,0 +1,32 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+ 
+const int pinCE = 9;
+const int pinCSN = 10;
+RF24 radio(pinCE, pinCSN);
+ 
+// Single radio pipe address for the 2 nodes to communicate.
+//const uint64_t pipe = 0xE8E8F0F0E1LL;
+const uint64_t pipe = 0xDEADBEEF00LL;
+ 
+char data[16];
+ 
+void setup(void)
+{
+   Serial.begin(9600);
+   radio.begin();
+   radio.openReadingPipe(1,pipe);
+   radio.startListening();
+   Serial.println("Recibiendo...!"); 
+}
+ 
+void loop(void)
+{
+   if (radio.available())
+   {
+      radio.read(data, 16); 
+      Serial.println(data);
+   }
+}
