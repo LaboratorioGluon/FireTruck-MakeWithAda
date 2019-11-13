cc90dd65b189a630612a8a5ec7bd8d5c0146a200
Angel <laboratoriogluon@gmail.com>
2019-11-13 20:16:15 +0100

    Servo second commit

--- AdaCode/src/main.adb
+++ AdaCode/src/main.adb
@@ -12,11 +12,12 @@ with STM32.Timers;
 
 with MainComms;
 with CarController;
-
+with Servo;
 
 procedure Main is
    use type Ada.Real_Time.Time;
    use type HAL.Uint8;
+   use type Servo.degree;
    Next_execution: Ada.Real_Time.Time;
    Period: constant Ada.Real_Time.Time_Span:= Ada.Real_Time.To_Time_Span(0.5);
 
@@ -30,13 +31,130 @@ procedure Main is
 
    pwm_mod : STM32.PWM.PWM_Modulator;
 
+   PumpPin    : STM32.GPIO.GPIO_Point := STM32.Device.PD4;
+   PumpPinNot : STM32.GPIO.GPIO_Point := STM32.Device.PD3;
+   
+   servo1 : Servo.Servo;
+   servo2 : Servo.Servo;
+   
+   servo_vertical: Servo.Servo renames servo2;
 
-
+   function Sine (Input : Long_Float) return Long_Float is
+         Pi : constant Long_Float := 3.14159_26535_89793_23846;
+         X  : constant Long_Float := Long_Float'Remainder (Input, Pi * 2.0);
+         B  : constant Long_Float := 4.0 / Pi;
+         C  : constant Long_Float := (-4.0) / (Pi * Pi);
+         Y  : constant Long_Float := B * X + C * X * abs (X);
+         P  : constant Long_Float := 0.225;
+      begin
+         return P * (Y * abs (Y) - Y) + Y;
+   end Sine;
+   
 begin
-
-   STM32.Board.Initialize_LEDs;
    STM32.Board.Configure_User_Button_GPIO;
+   STM32.Board.Initialize_LEDs;
+   
+   -- We need to enable and set to HIGH PA1 ASAP.
+   STM32.Device.Enable_Clock (PumpPin);
+   STM32.GPIO.Configure_IO (PumpPin,
+                            (Mode_Out,
+                             Resistors   => Floating,
+                             Output_Type => Push_Pull,
+                             Speed       => Speed_100MHz
+                            ));
+   PumpPin.Set;
+   STM32.Device.Enable_Clock (PumpPinNot);
+   STM32.GPIO.Configure_IO (PumpPinNot,
+                            (Mode_Out,
+                             Resistors   => Floating,
+                             Output_Type => Push_Pull,
+                             Speed       => Speed_100MHz
+                            ));
+   PumpPinNot.Clear;
+   
+   
+   servo1.Init(Pin            => STM32.Device.PB5,
+               Alternate_func => STM32.Device.GPIO_AF_TIM3_2,
+               Servo_Timer    => STM32.Device.Timer_3'access,
+               TimerChan      => STM32.Timers.Channel_2);
+   
+   servo1.setCalibration(Zero   => 1500,
+                         Top    => 2500,
+                         Bottom => 600);
+   
+   servo1.setLimits(Max_Degree => 30,
+                    Min_Degree => -30);
+   
+   servo2.Init(Pin            => STM32.Device.PB4,
+               Alternate_func => STM32.Device.GPIO_AF_TIM3_2,
+               Servo_Timer    => STM32.Device.Timer_3'access,
+               TimerChan      => STM32.Timers.Channel_1);
+   
+   servo2.setCalibration(Zero   => 1600,
+                         Top    => 2600,
+                         Bottom => 600);
+   
+   servo2.setLimits(Max_Degree => 15,
+                    Min_Degree => -30);
+   
 
+--     declare
+--        Arg       : Long_Float := 0.0;
+--        Value     : Integer;
+--        Increment : constant Long_Float := 0.1;
+--        Step : Integer := 400;
+--        Last_Step : Integer := -180;
+--        --  The Increment value controls the rate at which the brightness
+--        --  increases and decreases. The value is more or less arbitrary, but
+--        --  note that the effect of compiler optimization is observable.
+--     begin
+--        loop
+--  --           if STM32.Device.PA0.Set then
+--  --              --Value := Integer (180.0*Sine (Arg));
+--  --              Value := -180;
+--  --           else
+--  --              Value := 180;
+--  --           end if;
+--  
+--           if STM32.Device.PA0.Set and Last_Step = Step then
+--              if Step /= 0 then
+--                 Step := 0;
+--              else
+--                 Step := 1;
+--              end if;
+--              STM32.GPIO.Set(Green_LED);
+--           end if;
+--           
+--           if not STM32.Device.PA0.Set and Last_Step /= Step then
+--              Last_Step := Step;
+--              STM32.GPIO.Clear(Green_LED);
+--           end if;
+--           
+--           if Step > 2700 then
+--              Step := 400;
+--           end if;
+--           Value := Integer (90.0*Sine (Arg));
+--           if Step = 0 then
+--              servo1.setValue(Integer(servo1.calibration_zero));
+--           else
+--              servo1.setDegrees(Servo.degree(Value));
+--           end if;
+--           
+--           Value := Integer (90.0*Sine (Arg*2.0));
+--           if Step = 0 then
+--              servo2.setValue(Integer(servo2.calibration_zero));
+--           else
+--              servo2.setDegrees(Servo.degree(Value));
+--           end if;
+--           --servo1.setValue(400);
+--           Arg := Arg + Increment;
+--           
+--           delay 0.1;
+--        end loop;
+--     end;
+--        
+
+   
    -- PWM
    CarController.InitLeftMotor(LEFT_FWD     => STM32.Device.PE0,
                                LEFT_BCK     => STM32.Device.PE2,
@@ -78,13 +196,7 @@ begin
    end if;
 
 
-   STM32.Device.Enable_Clock (STM32.Device.PA1);
-   STM32.GPIO.Configure_IO (STM32.Device.PA1,
-                            (Mode_Out,
-                             Resistors   => Floating,
-                             Output_Type => Push_Pull,
-                             Speed       => Speed_100MHz
-                            ));
+  
 
    STM32.Device.Enable_Clock (STM32.Device.PD10);
    STM32.GPIO.Configure_IO (STM32.Device.PD10,
@@ -108,13 +220,15 @@ begin
    loop
 
 
---        if STM32.Device.PA0.Set then
---           STM32.GPIO.Set(Green_LED);
---           STM32.GPIO.Clear(STM32.Device.PA1);
---        else
---           STM32.GPIO.Set(STM32.Device.PA1);
---           STM32.GPIO.Clear (Green_LED);
---        end if;
+      if STM32.Device.PA0.Set then
+         STM32.GPIO.Set(Orange_LED);
+         STM32.GPIO.Clear(PumpPin);
+         STM32.GPIO.Set(PumpPinNot);
+      else
+         STM32.GPIO.Set(PumpPin);
+         STM32.GPIO.Clear(PumpPinNot);
+         STM32.GPIO.Clear (Orange_LED);
+      end if;
       --Console.putLine("Esperando..");
 
 --        Console.put("Data: ");
@@ -160,6 +274,9 @@ begin
                                    MainComms.getLastCommand(Tag => MainComms.SET_DIRECTION).Data(0))
                                 );
       CarController.setSpeed(CarController.Speed(MainComms.getLastCommand(Tag => MainComms.SET_SPEED).Data(0)));
+      
+      servo1.setDegrees(0);
+      servo2.setDegrees(0);
       STM32.GPIO.Set(Blue_LED);
 --        Console.putLine("Count: " & count'Img);
 --  --      Console.putLine("Data " & reg'Img);
